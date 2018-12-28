54428a5f3f1d6407d1f4c0278584a5597e99da80
James Mac <jameslee57822@gmail.com>
2018-12-28 16:22:19 -0700

    Added bootstrap, Navbar, Background Color

---
+++ Drifter-Motel-Website/Drifter-Motel-Website/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Drifter_Motel_Website.MvcApplication" Language="C#" %>
---
+++ Drifter-Motel-Website/Drifter-Motel-Website/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Drifter_Motel_Website.Startup))]
+namespace Drifter_Motel_Website
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
